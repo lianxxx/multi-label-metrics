a62daaf773aca7c62da28a9da91daf856bb1c7ef
Abhishek Verma <abhishek_verma@hotmail.com>
2017-08-17 08:50:46 +0530

    refactor: adding setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+     name='multilabel-metrics',             # This is the name of your PyPI-package.
+     version='0.1',                         # Update the version number for new releases.
+     scripts=['multilabel-metrics']         # The name of your scipt.
+)
