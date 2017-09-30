290e0a6dcbc54f90c9e912694447cdadebe00533
abhishek_verma@hotmail.com <abhishek_verma@hotmail.com>
2017-10-01 00:08:37 +0530

    refactor: change in filename

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     scripts=['multilabel-metrics'],
     author=u'Abhishek Verma',
     author_email='abhishek_verma@hotmail.com',
-    packages=['numpy'],
+    packages=['mlmetrics'],
     license='GNU_GPL licence, see LICENCE.txt',
     description='Multilabel classification metrics for Python',
     long_description=open('README.txt').read(),
