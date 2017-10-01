1939ed11761964fbe834ca280eba0cae534a4f09
abhishek_verma@hotmail.com <abhishek_verma@hotmail.com>
2017-10-01 18:06:42 +0530

    refactor: adding pip file

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     scripts=['multilabel-metrics'],
     author=u'Abhishek Verma',
     author_email='abhishek_verma@hotmail.com',
+	url='https://github.com/hell-sing/multi-label-metrics',
+	long_description=open('README.txt').read(),
+	py_modules=['mlmetrics'],
     license='GNU_GPL licence, see LICENCE.txt',
     description='Multilabel classification metrics for Python',
-    long_description=open('README.txt').read(),
-    zip_safe=False,
+	zip_safe=False
 )
