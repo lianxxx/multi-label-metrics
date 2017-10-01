69b4a2c70429dc4fc1b5381604620acced90e2ba
abhishek_verma@hotmail.com <abhishek_verma@hotmail.com>
2017-10-01 21:48:07 +0530

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 setup(
     name='multilabel-metrics',
     version='0.0.1',
@@ -9,5 +11,15 @@ setup(
     author=u'Abhishek Verma',
     author_email='abhishek_verma@hotmail.com',
     license='GNU_GPL licence, see LICENCE.txt',
-	py_modules=['mlmetrics']    
+	classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='classification metrics machine-learning ',
+    py_modules=["mlmetrics"],
 )
