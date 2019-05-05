e2f5322fec55ae7f1d4a65b59a60a302be7ad5b3
llabhishekll <abhishek_verma@hotmail.com>
2019-05-05 12:19:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.0.1',
     description='Multilabel classification metrics for Python',
     long_description=open('README.txt').read(),
-    url='https://github.com/hell-sing/multi-label-metrics',
+    url='https://github.com/llabhishekll/multi-label-metrics',
     author=u'Abhishek Verma',
     author_email='abhishek_verma@hotmail.com',
     license='GNU_GPL licence, see LICENCE.txt',
